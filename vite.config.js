47072a8070b2fcb3d4580c3368433cb57573dbab
Zgrowth <141380129+Zgrowth@users.noreply.github.com>
2024-07-11 11:24:27 +0800

    fix：更新版本号

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
         description: '在HiFiNi网站自动播放歌曲，可以自定义播放列表',
         match: ['https://www.hifini.com/*'],
         author: 'zs',
-        version: '1.0.0',
+        version: '1.0.2',
         require: [
           'https://cdn.jsdelivr.net/npm/dayjs@1.11.11/dayjs.min.js'
         ],
